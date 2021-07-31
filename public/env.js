6b0f1045be14bc55430b53beb154a37b11346a3d
nandugangavaram <nandugangavaram@gmail.com>
2021-07-31 17:45:11 +0530

    Added and Modified about.html file with basic template

--- public/env.js
+++ public/env.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  // var instances = M.FloatingActionButton.init(elems, options);
-});
-
 var generatePosts = (items) => {
   
   let receipes = document.getElementById("Receipes");
--- server.js
+++ server.js
@@ -8,9 +8,8 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/test", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
+app.get("/about", function (request, response) {
+  response.sendFile(__dirname + "/public/about.html");
 });
 
 //socket test
