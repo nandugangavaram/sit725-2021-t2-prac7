1102208c614e521d4e390f3128e30154b1a9f959
nandugangavaram <nandugangavaram@gmail.com>
2021-08-21 19:47:51 +0530

    Update About Page in routes folder

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,30 +1,21 @@
 var express =  require('express');
 var router = express.Router();
 var controllers = require('../controllers');
+var path = require("path");
 
 router.post("/addRecipe", (req, res) => {
     console.log("ADD POST Method Routes");
     controllers.addRecipeController.addRecipe(req, res);
-    // var newRecipe = req.body;
-    // insertRecipe(newRecipe, (err, result) => {
-    //     if(err) {
-    //     res.json({status: 400, message:err});
-    //     } else {
-    //     res.json({statusCode: 200, message:"Project Successfully added", data: result});
-    //     }
-    // });
 });
   
 router.get("/posts", (req, res) => {
     console.log("GET POSTS Method Routes");
-    controllers.postsController.getRecipes(req, res);
-    // getRecipes((err, result) => {
-    //     if(err) {
-    //     res.json({status: 400, message:err});
-    //     } else {
-    //     res.json({statusCode: 200, message:"Success", data: result});
-    //     }
-    // });
+    controllers.postsController.getRecipes(req, res);    
+});
+
+router.get("/about", function (request, response) {
+    console.log(__dirname);
+    response.sendFile('/about.html', { root: path.join(__dirname, "..",  "public" )});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,22 +11,11 @@ var port = process.env.PORT || 5500;
 // routes
 let projectsRoute = require('./routes/recipes')
 
+// middlewares
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/', projectsRoute);
-
-app.get("/about", function (request, response) {
-  response.sendFile(__dirname + "/public/about.html");
-});
-
-// //socket test
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
 
 app.listen(port, () => {
   console.log("Listening on port ", port);
